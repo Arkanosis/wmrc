8d9f1724059b53cfe4d75c092453b5e788a29f35
Jérémie Roquet <jroquet@arkanosis.net>
2018-09-04 01:09:01 +0200

    Switch to next diff when pressing revert or patrol

--- app/src/main/kotlin/net/arkanosis/wmrc/activities/MainActivity.kt
+++ app/src/main/kotlin/net/arkanosis/wmrc/activities/MainActivity.kt
@@ -2,6 +2,7 @@ package net.arkanosis.wmrc.activities
 
 import android.content.Intent
 import android.os.Bundle
+import android.support.v7.widget.AppCompatButton
 import android.view.Menu
 import android.view.MenuItem
 
@@ -12,13 +13,32 @@ import net.arkanosis.wmrc.R
 
 class MainActivity : BaseActivity() {
 
+    private val diffs = arrayOf(R.raw.diff1, R.raw.diff2)
+
+    private var currentDiff = 0
+
+    private fun switchDiff() {
+        web_view_id.loadDataWithBaseURL(null, getResources().openRawResource(diffs[currentDiff]).reader().use { it.readText() }, "text/html", "utf-8", null)
+        currentDiff = (currentDiff + 1) % diffs.size
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar);
-        web_view_id.loadData(getResources().openRawResource(R.raw.diff).reader().use { it.readText() }, "text/html; charset=utf-8", null)
         web_view_id.setInitialScale(1)
         web_view_id.getSettings().setUseWideViewPort(true)
+        switchDiff()
+
+        val revertButton = findViewById(R.id.revert_id) as AppCompatButton
+        val onClickListener: Any = revertButton.setOnClickListener {
+            switchDiff()
+        }
+
+        val approveButton = findViewById(R.id.patrol_id) as AppCompatButton
+        approveButton.setOnClickListener {
+            switchDiff()
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
