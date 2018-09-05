24b0f39bf724fb8998d4f4585fa14c669a8a88da
Jérémie Roquet <jroquet@arkanosis.net>
2018-09-06 00:19:38 +0200

    Re-implement the about activity without third-party dependency

--- app/src/main/kotlin/net/arkanosis/wmrc/activities/AboutActivity.kt
+++ app/src/main/kotlin/net/arkanosis/wmrc/activities/AboutActivity.kt
@@ -1,25 +1,21 @@
 package net.arkanosis.wmrc.activities
 
 import android.os.Bundle
+import android.widget.TextView
 
-import mehdi.sakout.aboutpage.AboutPage
-import mehdi.sakout.aboutpage.Element
+import kotlinx.android.synthetic.main.activity_about.*
 
 import net.arkanosis.wmrc.BuildConfig
+import net.arkanosis.wmrc.R
 
 class AboutActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val aboutPage = AboutPage(this)
-                .isRTL(false)
-                .addItem(Element().setTitle("wmrc v" + BuildConfig.VERSION_NAME))
-                .setDescription("THIS IS A SUPER SECRET PROJECT AND I WON'T TELL YOU ANYTHING ABOUT IT and btw, why have you installed something on you device if you don't know what it does?!")
-                .addEmail("jroquet@arkanosis.net")
-                .addWebsite("https://wmrc.arkanosis.net/")
-                .addGitHub("Arkanosis/wmrc")
-                .create()
-        setContentView(aboutPage)
+        setContentView(R.layout.activity_about)
+        setSupportActionBar(toolbar)
+        val versionLabel = findViewById(R.id.about_version) as TextView
+        versionLabel.setText("v" + BuildConfig.VERSION_NAME)
     }
 
 }
